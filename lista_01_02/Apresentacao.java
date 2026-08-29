e0f84880506f1184bb0a9c500d9994cc3d4727d9
Bruno André Fernandes Ortega <fernandes.bruno0609@gmail.com>
2026-08-29 15:59:42 -0300

    Added Lists 01 through 04

---
+++ lista_01_02/Apresentacao.java
@@ -0,0 +1,7 @@
+package lista_01_02;
+
+public class Apresentacao {
+    public static void main(String[] args) {
+        System.out.println("Olá, meu nome é Bruno Ortega! Missão 00 concluída com sucesso.");
+    }
+}
