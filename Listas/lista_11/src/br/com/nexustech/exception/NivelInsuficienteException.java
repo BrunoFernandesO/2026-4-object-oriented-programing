0d901770ff8b2c8e2e114f81d8cf96d18c8b8b75
Bruno André Fernandes Ortega <fernandes.bruno0609@gmail.com>
2026-09-13 14:54:02 -0300

    Added lista_11

---
+++ Listas/lista_11/src/br/com/nexustech/exception/NivelInsuficienteException.java
@@ -0,0 +1,7 @@
+package br.com.nexustech.exception;
+
+public class NivelInsuficienteException extends RuntimeException {
+    public NivelInsuficienteException() {
+        super("Seu nivel e muito baixo para esta masmorra!");
+    }
+}
---
+++ Listas/lista_11/src/br/com/nexustech/model/ModoCasual.java
@@ -0,0 +1,7 @@
+package br.com.nexustech.model;
+
+public class ModoCasual implements ModoJogo {
+    public void buscarPartida() {
+        System.out.println("Buscando partida casual sem estresse...");
+    }
+}
